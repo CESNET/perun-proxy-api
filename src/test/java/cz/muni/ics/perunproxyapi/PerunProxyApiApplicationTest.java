0e415f9335030f4de457a9d461133439bc082c50
Pavel Vyskočil <vyskocilpavel@muni.cz>
2020-07-21 10:58:27 +0200

    Added mapping service

---
+++ src/test/java/cz/muni/ics/perunproxyapi/PerunProxyApiApplicationTest.java
@@ -0,0 +1,23 @@
+package cz.muni.ics.perunproxyapi;
+
+import cz.muni.ics.perunproxyapi.attributes.AttributeMappingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+public class PerunProxyApiApplicationTest {
+
+    @Autowired
+    AttributeMappingService ams;
+
+    @Test
+    public void testMappingConfig() {
+        assertNotNull(ams);
+        assertEquals(2, ams.getAttributeMap().keySet().size(), "Should load 2 AttributeObjectMapping objects");
+    }
+}
